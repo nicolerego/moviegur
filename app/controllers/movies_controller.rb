157bb99133b047afe14a9d161e11fe6c8bac17c9
Nicole Rego <nicole.rego@live.com>
2015-02-04 14:59:31 -0500

    inital commit

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,47 @@
+class MoviesController < ApplicationController
+	def index
+		@movies = Movie.all
+	end 
+
+	def show
+		@movie = Movie.find(params[:id])
+	end 
+
+	def new
+		@movie = Movie.new
+	end 
+
+	def create 
+		@movie = Movie.new(movie_params)
+		if @movie.save
+			redirect_to movies_url
+		else
+			render :new
+		end
+	end 
+
+	def edit
+		@movie = Movie.find(params[:id])
+	end 
+
+	def update 
+		@movie = Movie.find(params[:id])
+		if @movie.update_attributes(movie_params)
+			# redirect_to "/movies/#{@movie.id}"
+			redirect_to movie_path(@movie)
+		else 
+			render :edit
+		end 
+	end 
+
+	def destroy
+		@movie = Movie.find(params[:id])
+		@movie.destroy
+		redirect_to movies_url
+	end 
+
+	private
+	def movie_params
+		params.require(:movie).permit(:title, :director, :rating)
+	end 
+end
---
+++ db/seeds.rb
@@ -0,0 +1,24 @@
+Movie.create!(
+	:title => "Back to The Future",
+	:director => "Robert Zemeckis",
+	:rating => 5
+)
+
+Movie.create!(
+	:title => "Army of Darkness",
+	:director => "Sam Raimi",
+	:rating => 4
+)
+
+Movie.create!(
+	:title => "The Hobbit: The Battle of the Five Armies",
+	:director => "Peter Jackson",
+	:rating => 3
+)
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
