c78f43d88f38f06a87e96aec73a768aee747bcce
Nicole Rego <nicole.rego@live.com>
2015-02-07 08:23:57 -0500

    Adding slug to movie url

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,13 @@ class Movie < ActiveRecord::Base
 	scope :newest_first, -> { order(created_at: :DESC) }
  	scope :most_recent_three, -> { newest_first.limit(3) }
  	scope :created_before, ->(time) { where("created_at < ?", time) }
+
+ 	def slug
+    	title.downcase.gsub(' ', '-')  
+  	end
+
+  	def to_param
+    	"#{id}-#{slug}"
+  	end
+
 end
